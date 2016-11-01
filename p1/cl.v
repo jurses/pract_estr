65be8e7664634fb453bad2502bb2c09e3e0dc203
Raúl U <martin.jurses@gmail.com>
2016-11-01 10:11:32 +0000

    Arreglado las conexiones de cl y el muy
    Arreglé las conexiones de la celda lógica y el multiplexor

--- p1/cl.v
+++ p1/cl.v
@@ -1,11 +1,7 @@
-module cl(output reg out, input wire a, b, input wire [1:0] s);
+module cl(output wire out, input wire a, b, input wire [1:0] s);
 wire s_and, s_or, s_xor, s_not;
-output wire mux_out;
 
-assign mux_out = out;
-
-mux4_1 mux_cl(mux_out, 1'b0, 1'b1, 1'b1, s_not, 2'b01);
-
+mux4_1 mux_cl(out, s_and, s_or, s_xor, s_not, s);
 
 and(s_and,a,b); // 00
 or(s_or,a,b);   // 01
